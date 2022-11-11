430ef658a3c9a609aa8875d6dbaaf90e93ae0dc2
Daniel Kritzinger <danielkritzinger@gmail.com>
2022-11-11 11:01:20 +0100

    Wodbot first commit
    Built POC version of wodbot which consists of a crossfit wod scraper function and a telegrambot notifier function.

---
+++ README.md
@@ -0,0 +1,13 @@
+# WodBot
+
+## Project description:
+
+This utility aims to provide easy to access crossfit/cross-training daily workouts.
+
+## TODO:
+- [ x ] Build crossfit wod scraper.
+- [ x ] Build telegrambot notifier.
+- [ ] Host solution with cronjob.
+- [ ] Add warmup and warmdown exercises to wod sessions.
+- [ ] Store solutions in database.
+- [ ] Build alternate recommender.
---
+++ src/index.js
@@ -0,0 +1,60 @@
+const axios = require('axios');
+const moment = require('moment');
+
+require('dotenv').config()
+
+const crossfitWOD = async () => {
+
+    const { data } = await axios.get(
+        'https://www.crossfit.com/workout/'
+    );
+    
+    const date = moment();
+    const wodDateId = `w${date.format('YYYYMMDD')}`;
+
+    if (data.wods[0].id === wodDateId) {
+        const wod = {};
+        wod.id =  data.wods[0].cleanID;
+        wod.title = data.wods[0].title; 
+        wod.content = data.wods[0].wodRaw;
+        return wod;
+    } else {
+        return {};
+    };
+};
+
+const contentEditor = async (contentTitle, content) => {
+    var message = `**${contentTitle}** \n\n ${content}`
+
+    // var escapeChars = ['_', '[', ']', '(', ')', '~', '`', '>', '#', '+', '-', '=', '|', '{', '}', '.', '!'];
+    // for (let i=0; i<escapeChars.length; i++ ) {
+    //     var message = message.replaceAll(escapeChars[i], `\\${escapeChars[i]}`); 
+    // };
+    
+    return encodeURI(message);
+};
+
+const telegramNotifier = async (message) => {
+
+    let urlString = `https://api.telegram.org/bot${process.env.DEVWODBOT_APIKEY}/sendMessage?chat_id=${process.env.DEVWODBOT_GROUPCHATID}&text=${message}`;
+    return axios.get(urlString)
+            .then(response => response.data)
+            .catch(error => {
+                console.log(error);
+            });
+};
+
+const Wodbot = async () => {
+    try {
+        var wod = await crossfitWOD()
+        var message = await contentEditor(wod.title, wod.content)
+        var resp = await telegramNotifier(message)
+
+        return resp;
+    } catch (error) {
+        throw error;
+    };
+};
+
+Wodbot()
+    .then((resp) => console.log(resp));
